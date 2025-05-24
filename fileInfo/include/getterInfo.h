3e3e6928d7907fa3ea4d4fe53352475e22ab3a99
lvbealr <lvbealr@mail.ru>
2025-05-24 21:59:30 +0300

    implement info getter

---
+++ fileInfo/include/getterInfo.h
@@ -0,0 +1,38 @@
+#ifndef GETTER_INFO_H_
+#define GETTER_INFO_H_
+
+#include <sys/stat.h>
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+enum getterInfoStatus {
+    GETTER_INFO_ERROR     = -1,
+    NO_ERRORS             =  0,
+    BAD_FILE_NAME         =  1,
+    FILE_BAD_POINTER      =  2,
+    ALLOCATION_ERROR      =  3,
+    FILE_INFO_BAD_POINTER =  4,
+    PRINT_INFO_ERROR      =  5,
+    DELETE_INFO_ERROR     =  6
+};
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+struct fileInfo {
+           char *fileName = {};
+    struct stat *info     = {};
+};
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+fileInfo *setFile(const char *fileName);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+getterInfoStatus getFileInfo   (fileInfo *file);
+getterInfoStatus printFileInfo (fileInfo *file);
+getterInfoStatus deleteFileInfo(fileInfo *file);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+#endif // GETTER_INFO_H_
---
+++ fileInfo/src/getterInfo.cpp
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+
+#include "getterInfo.h"
+#include "customWarning.h"
+
+#include <string.h>
+
+#define FREE_(ptr) do { \
+    free(ptr);          \
+    ptr = NULL;         \
+} while (0)
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+fileInfo *setFile(const char *fileName) {
+    customWarning(fileName, NULL);
+
+    fileInfo *file = (fileInfo *)calloc(1, sizeof(fileInfo));
+    customWarning(file, NULL);
+
+    size_t fileNameLength = strlen(fileName);
+
+    file->fileName = (char *)calloc(fileNameLength + 1, sizeof(char));
+    customWarning(file->fileName, NULL);
+
+    strncpy(file->fileName, fileName, fileNameLength);
+    file->fileName[fileNameLength] = '\0';
+
+    file->info = NULL;
+
+    return file;
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+getterInfoStatus getFileInfo(fileInfo *file) {
+    customWarning(file,           FILE_BAD_POINTER);
+    customWarning(file->fileName, BAD_FILE_NAME);
+
+    file->info = (struct stat *)calloc(1, sizeof(struct stat));
+    customWarning(file->info, ALLOCATION_ERROR);
+
+    getterInfoStatus status = (getterInfoStatus)stat(file->fileName, file->info);
+    customWarning(status == NO_ERRORS, GETTER_INFO_ERROR);
+
+    return NO_ERRORS;
+};
+
+getterInfoStatus printFileInfo(fileInfo *file) {
+    customWarning(file,           FILE_BAD_POINTER);
+    customWarning(file->fileName, BAD_FILE_NAME);
+    customWarning(file->info,     FILE_INFO_BAD_POINTER);
+
+    customPrint(purple, bold, bgDefault, "File: %s\n", file->fileName);
+
+    // --------------------------------------------------- stat ------------------------------------------------- //
+    customPrint(white, bold, bgDefault, "st_dev (device): %lu\n",                    file->info->st_dev);
+    customPrint(white, bold, bgDefault, "st_ino (inode): %lu\n",                     file->info->st_ino);
+    customPrint(white, bold, bgDefault, "st_mode (mode): %o\n",                      file->info->st_mode);
+    customPrint(white, bold, bgDefault, "st_nlink (number of links): %lu\n",         file->info->st_nlink);
+    customPrint(white, bold, bgDefault, "st_uid (user ID): %u\n",                    file->info->st_uid);
+    customPrint(white, bold, bgDefault, "st_gid (group ID): %u\n",                   file->info->st_gid);
+    customPrint(white, bold, bgDefault, "st_rdev (device type): %lu\n",              file->info->st_rdev);
+    customPrint(white, bold, bgDefault, "st_size (size in bytes): %ld\n",            file->info->st_size);
+    customPrint(white, bold, bgDefault, "st_blksize (block size): %ld\n",            file->info->st_blksize);
+    customPrint(white, bold, bgDefault, "st_blocks (number of blocks): %ld\n",       file->info->st_blocks);
+    customPrint(white, bold, bgDefault, "st_atime (last access time): %ld\n",        file->info->st_atime);
+    customPrint(white, bold, bgDefault, "st_mtime (last modification time): %ld\n",  file->info->st_mtime);
+    customPrint(white, bold, bgDefault, "st_ctime (last status change time): %ld\n", file->info->st_ctime);
+    // ---------------------------------------------------------------------------------------------------------- //
+
+    return NO_ERRORS;
+}
+
+getterInfoStatus deleteFileInfo(fileInfo *file) {
+    customWarning(file, FILE_BAD_POINTER);
+
+    if (file->fileName) {
+        FREE_(file->fileName);
+    }
+
+    if (file->info) {
+        FREE_(file->info);
+    }
+
+    FREE_(file);
+
+    return NO_ERRORS;
+}
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------- //
